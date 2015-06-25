80ef7962b12c84f1414e2e4681109692fcfae479
mateyCompany <matey.company@gmail.com>
2015-06-25 15:12:55 +0900

    기본

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	/* Gnb-Lnb */
+	$('#Gnb ul li').eq(gVar).addClass('on');
+	$('#Lnb .lnb-menu > li').eq(lVar).addClass('on').find('ul li').eq(lsVar).addClass('on');
+
+
+	
+	
+	
+	
+
+}); //end document ready
+
+
+
