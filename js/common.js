28d7f8e07cc4b02f14d0697c52c7cfb6807ea0ba
mateyCompany <matey.company@gmail.com>
2015-06-26 17:04:12 +0900

    안준상

--- js/common.js
+++ js/common.js
@@ -13,3 +13,38 @@ $(document).ready(function(){
 
 
 
+
+// 설렉트박스 스크립트(리펙토리전상태)
+function selecter(tar){
+	var $selecter = $(tar);
+	var $tarUL = $selecter.find('ul');
+	var $activeTar = $tarUL.find('a');
+	var $activeTxt = $selecter.find('.activeTxt');
+	var tarWidth = $selecter.width();
+	var tarULWidth = $tarUL.width();
+	var $reset = $tarUL.find('li:first-child a');
+
+	$activeTxt.text($reset.text());
+	$activeTxt.attr('data-role',$reset.attr('data-role'));
+	$selecter.width(tarULWidth-1);
+
+	$selecter.click(function(){
+		$(this).find('ul').toggleClass('on');
+	});
+	$activeTar.click(function(){
+		var $active = $(this);
+		var activeData = $active.attr('data-role');
+		$activeTxt.text($active.text());
+		$activeTxt.attr('data-role',activeData);
+		$tarUL.removeClass('on');
+		return false;
+	});
+
+	$selecter.hover(function(){},function(){
+		if($tarUL.hasClass('on')){
+			$tarUL.removeClass('on');
+		}
+	});
+
+};
+
